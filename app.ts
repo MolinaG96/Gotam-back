e3e625cacc2055997093d6958623fb32f3d55c5d
Gonzalo <molinagomezgonzalo@gmail.com>
2023-10-10 21:59:33 -0300

    feat: db created

---
+++ app.ts
@@ -0,0 +1,29 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import { connectDB } from './config/db'
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(
+    cors({
+        origin: ['http://localhost:3000'],
+        credentials: true,
+    })
+)
+
+connectDB()
+    .then(() => {
+        const PORT = process.env.PORT ?? 443
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error)
+        process.exit(1)
+    })
+
+export { app }
